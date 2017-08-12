7577c4657ffaac52e4f4c97110f6c6dd639d60e9
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-08-12 15:01:03 +0100

    Add methods

--- src/main/java/com/error22/karonda/ir/KClass.java
+++ src/main/java/com/error22/karonda/ir/KClass.java
@@ -1,15 +1,28 @@
 package com.error22.karonda.ir;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class KClass implements IType {
 	private String name, superName;
 	private ClassType type;
 	private boolean specialResolve;
-	
+	private Map<MethodSignature, KMethod> methods;
+
 	public KClass(String name, ClassType type, boolean specialResolve, String superName) {
 		this.name = name;
 		this.type = type;
 		this.specialResolve = specialResolve;
 		this.superName = superName;
+		methods = new ConcurrentHashMap<MethodSignature, KMethod>();
+	}
+
+	public void addMethod(KMethod method) {
+		methods.put(method.getSignature(), method);
+	}
+
+	public KMethod getMethod(MethodSignature signature) {
+		return methods.get(signature);
 	}
 
 }
---
+++ src/main/java/com/error22/karonda/ir/KMethod.java
@@ -0,0 +1,14 @@
+package com.error22.karonda.ir;
+
+public class KMethod {
+	private MethodSignature signature;
+
+	public KMethod(MethodSignature signature) {
+		this.signature = signature;
+	}
+
+	public MethodSignature getSignature() {
+		return signature;
+	}
+
+}
