21182fb33fc4c2c862355f287cc86c2b2a036c00
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-08-12 14:02:47 +0100

    Add not implemented exception

---
+++ src/main/java/com/error22/karonda/NotImplementedException.java
@@ -0,0 +1,13 @@
+package com.error22.karonda;
+
+public class NotImplementedException extends RuntimeException {
+	private static final long serialVersionUID = -2696308466651195634L;
+
+	public NotImplementedException() {
+	}
+
+	public NotImplementedException(String msg) {
+		super(msg);
+	}
+
+}
