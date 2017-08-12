5857212ef42a360683c385857d78989c950aef38
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-08-12 22:17:20 +0100

    Add opcode checking

--- src/main/java/com/error22/karonda/converter/MethodConverter.java
+++ src/main/java/com/error22/karonda/converter/MethodConverter.java
@@ -1,8 +1,13 @@
 package com.error22.karonda.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.objectweb.asm.Label;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.commons.InstructionAdapter;
 
+import com.error22.karonda.NotImplementedException;
 import com.error22.karonda.ir.KMethod;
 
 public class MethodConverter extends InstructionAdapter {
@@ -12,5 +17,76 @@ public class MethodConverter extends InstructionAdapter {
 		super(Opcodes.ASM5, null);
 		this.kMethod = kMethod;
 	}
-	
+
+	@Override
+	public void visitInsn(int opcode) {
+		if (!SUPPORTED_OPS.contains(opcode))
+			throw new NotImplementedException("OP: " + opcode);
+		super.visitInsn(opcode);
+	}
+
+	@Override
+	public void visitFieldInsn(int opcode, String owner, String name, String desc) {
+		if (!SUPPORTED_OPS.contains(opcode))
+			throw new NotImplementedException("OP: " + opcode);
+		super.visitFieldInsn(opcode, owner, name, desc);
+	}
+
+	@Override
+	public void visitIntInsn(int opcode, int operand) {
+		if (!SUPPORTED_OPS.contains(opcode))
+			throw new NotImplementedException("OP: " + opcode);
+		super.visitIntInsn(opcode, operand);
+	}
+
+	@Override
+	public void visitJumpInsn(int opcode, Label label) {
+		if (!SUPPORTED_OPS.contains(opcode))
+			throw new NotImplementedException("OP: " + opcode);
+		super.visitJumpInsn(opcode, label);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void visitMethodInsn(int opcode, String owner, String name, String desc) {
+		if (!SUPPORTED_OPS.contains(opcode))
+			throw new NotImplementedException("OP: " + opcode);
+		super.visitMethodInsn(opcode, owner, name, desc);
+	}
+
+	@Override
+	public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
+		if (!SUPPORTED_OPS.contains(opcode))
+			throw new NotImplementedException("OP: " + opcode);
+		super.visitMethodInsn(opcode, owner, name, desc, itf);
+	}
+
+	@Override
+	public void visitTypeInsn(int opcode, String type) {
+		if (!SUPPORTED_OPS.contains(opcode))
+			throw new NotImplementedException("OP: " + opcode);
+		super.visitTypeInsn(opcode, type);
+	}
+
+	@Override
+	public void visitVarInsn(int opcode, int var) {
+		if (!SUPPORTED_OPS.contains(opcode))
+			throw new NotImplementedException("OP: " + opcode);
+		super.visitVarInsn(opcode, var);
+	}
+
+	@Override
+	public void goTo(Label label) {
+		throw new NotImplementedException("goto " + label);
+	}
+
+	@Override
+	public void visitLabel(Label label) {
+		throw new NotImplementedException("label " + label);
+	}
+
+	private static List<Integer> SUPPORTED_OPS;
+	static {
+		SUPPORTED_OPS = new ArrayList<Integer>();
+	}
 }
