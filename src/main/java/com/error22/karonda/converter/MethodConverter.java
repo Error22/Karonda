89505c21377f31444599fcea160cf7969ba398f4
Chandler Newman <chandler2newman@hotmail.co.uk>
2017-08-13 11:53:25 +0100

    Move to method visitor

--- src/main/java/com/error22/karonda/converter/MethodConverter.java
+++ src/main/java/com/error22/karonda/converter/MethodConverter.java
@@ -6,14 +6,14 @@ import java.util.List;
 import java.util.Map;
 
 import org.objectweb.asm.Label;
+import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.commons.InstructionAdapter;
 
 import com.error22.karonda.NotImplementedException;
 import com.error22.karonda.instructions.IInstruction;
 import com.error22.karonda.ir.KMethod;
 
-public class MethodConverter extends InstructionAdapter {
+public class MethodConverter extends MethodVisitor {
 	private KMethod kMethod;
 	private ArrayList<IInstruction> instructions;
 	private Map<Label, Integer> labelMap;
@@ -25,66 +25,55 @@ public class MethodConverter extends InstructionAdapter {
 		labelMap = new HashMap<Label, Integer>();
 	}
 
+	private void addInstruction(IInstruction instruction) {
+		instructions.add(instruction);
+	}
+
 	@Override
 	public void visitInsn(int opcode) {
 		if (!SUPPORTED_OPS.contains(opcode))
 			throw new NotImplementedException("OP: " + opcode);
-		super.visitInsn(opcode);
 	}
 
 	@Override
 	public void visitFieldInsn(int opcode, String owner, String name, String desc) {
 		if (!SUPPORTED_OPS.contains(opcode))
 			throw new NotImplementedException("OP: " + opcode);
-		super.visitFieldInsn(opcode, owner, name, desc);
 	}
 
 	@Override
 	public void visitIntInsn(int opcode, int operand) {
 		if (!SUPPORTED_OPS.contains(opcode))
 			throw new NotImplementedException("OP: " + opcode);
-		super.visitIntInsn(opcode, operand);
 	}
 
 	@Override
 	public void visitJumpInsn(int opcode, Label label) {
 		if (!SUPPORTED_OPS.contains(opcode))
 			throw new NotImplementedException("OP: " + opcode);
-		super.visitJumpInsn(opcode, label);
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void visitMethodInsn(int opcode, String owner, String name, String desc) {
-		if (!SUPPORTED_OPS.contains(opcode))
-			throw new NotImplementedException("OP: " + opcode);
-		super.visitMethodInsn(opcode, owner, name, desc);
+		throw new UnsupportedOperationException("Use new ASM5 visitMethodInsn method");
 	}
 
 	@Override
 	public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
 		if (!SUPPORTED_OPS.contains(opcode))
 			throw new NotImplementedException("OP: " + opcode);
-		super.visitMethodInsn(opcode, owner, name, desc, itf);
 	}
 
 	@Override
 	public void visitTypeInsn(int opcode, String type) {
 		if (!SUPPORTED_OPS.contains(opcode))
 			throw new NotImplementedException("OP: " + opcode);
-		super.visitTypeInsn(opcode, type);
 	}
 
 	@Override
 	public void visitVarInsn(int opcode, int var) {
 		if (!SUPPORTED_OPS.contains(opcode))
 			throw new NotImplementedException("OP: " + opcode);
-		super.visitVarInsn(opcode, var);
-	}
-
-	@Override
-	public void goTo(Label label) {
-		throw new NotImplementedException("goto " + label);
 	}
 
 	@Override
