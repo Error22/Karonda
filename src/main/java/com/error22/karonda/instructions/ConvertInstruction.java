b674427466d36b5076b5079f4f7e76c9e88ccd63
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-02-21 20:59:52 +0000

    Convert convertinstruction to use ints

--- src/main/java/com/error22/karonda/instructions/ConvertInstruction.java
+++ src/main/java/com/error22/karonda/instructions/ConvertInstruction.java
@@ -1,7 +1,6 @@
 package com.error22.karonda.instructions;
 
 import com.error22.karonda.NotImplementedException;
-import com.error22.karonda.ir.PrimitiveObject;
 import com.error22.karonda.ir.PrimitiveType;
 import com.error22.karonda.vm.StackFrame;
 
@@ -15,31 +14,52 @@ public class ConvertInstruction implements IInstruction {
 
 	@Override
 	public void execute(StackFrame stackFrame) {
-		Object pvalue = ((PrimitiveObject) stackFrame.pop()).getValue();
-		switch (to) {
+		Number value;
+		switch (from) {
 		case Byte:
-			stackFrame.push(new PrimitiveObject(PrimitiveType.Int, (int) ((Number) pvalue).byteValue()));
-			break;
 		case Boolean:
-			stackFrame.push(new PrimitiveObject(PrimitiveType.Int, (int) ((Number) pvalue).intValue() != 0));
-			break;
 		case Char:
-			stackFrame.push(new PrimitiveObject(PrimitiveType.Int, (int) (char) ((Number) pvalue).intValue()));
-			break;
 		case Short:
-			stackFrame.push(new PrimitiveObject(PrimitiveType.Int, (int) ((Number) pvalue).shortValue()));
-			break;
 		case Int:
-			stackFrame.push(new PrimitiveObject(PrimitiveType.Int, (int) ((Number) pvalue).byteValue()));
+			value = stackFrame.pop();
 			break;
 		case Long:
-			stackFrame.push(new PrimitiveObject(PrimitiveType.Long, ((Number) pvalue).longValue()));
+			value = stackFrame.popLong();
 			break;
 		case Float:
-			stackFrame.push(new PrimitiveObject(PrimitiveType.Float, ((Number) pvalue).floatValue()));
+			value = Float.intBitsToFloat(stackFrame.pop());
 			break;
 		case Double:
-			stackFrame.push(new PrimitiveObject(PrimitiveType.Double, ((Number) pvalue).doubleValue()));
+			value = Double.longBitsToDouble(stackFrame.popLong());
+			break;
+		default:
+			throw new NotImplementedException();
+		}
+
+		switch (to) {
+		case Byte:
+			stackFrame.push(value.byteValue());
+			break;
+		case Boolean:
+			stackFrame.push(value.intValue() != 0 ? 1 : 0);
+			break;
+		case Char:
+			stackFrame.push((int) (char) value.intValue());
+			break;
+		case Short:
+			stackFrame.push(value.shortValue());
+			break;
+		case Int:
+			stackFrame.push(value.intValue());
+			break;
+		case Long:
+			stackFrame.push(value.longValue());
+			break;
+		case Float:
+			stackFrame.push(Float.floatToIntBits(value.floatValue()));
+			break;
+		case Double:
+			stackFrame.push(Double.doubleToRawLongBits(value.doubleValue()));
 			break;
 		default:
 			throw new NotImplementedException();
