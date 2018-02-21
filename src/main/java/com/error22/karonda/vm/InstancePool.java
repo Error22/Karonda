78ad8ba7db2415a2bc4b6f03e76f2205978f1506
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-02-21 20:14:14 +0000

    Convert instancepool to use int based ids

--- src/main/java/com/error22/karonda/vm/InstancePool.java
+++ src/main/java/com/error22/karonda/vm/InstancePool.java
@@ -1,35 +1,40 @@
 package com.error22.karonda.vm;
 
-import java.util.HashMap;
+import java.util.BitSet;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.javatuples.Pair;
 
 import com.error22.karonda.ir.ArrayType;
 import com.error22.karonda.ir.FieldSignature;
-import com.error22.karonda.ir.IObject;
 import com.error22.karonda.ir.IType;
 import com.error22.karonda.ir.KClass;
 import com.error22.karonda.ir.KField;
 import com.error22.karonda.ir.KMethod;
 import com.error22.karonda.ir.MethodSignature;
-import com.error22.karonda.ir.ObjectReference;
 import com.error22.karonda.ir.PrimitiveType;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+
 public class InstancePool {
 	private Map<KClass, Boolean> staticInitMap;
-	private Map<FieldSignature, IObject> staticFields;
-	private Map<UUID, ObjectInstance> objects;
-	private BiMap<Pair<IType, Object>, UUID> runtimeClasses;
+	private Object2ObjectOpenHashMap<FieldSignature, int[]> staticFields;
+	private Int2ObjectOpenHashMap<ObjectInstance> objects;
+	private BitSet objectIds;
+	private int idHint;
+	private BiMap<Pair<IType, Object>, Integer> runtimeClasses;
 
 	public InstancePool() {
 		staticInitMap = new ConcurrentHashMap<KClass, Boolean>();
-		staticFields = new HashMap<FieldSignature, IObject>();
-		objects = new HashMap<UUID, ObjectInstance>();
+		staticFields = new Object2ObjectOpenHashMap<FieldSignature, int[]>();
+		objects = new Int2ObjectOpenHashMap<ObjectInstance>();
+		objectIds = new BitSet();
+		objectIds.set(0);
+		idHint = 1;
 		runtimeClasses = HashBiMap.create();
 	}
 
@@ -50,46 +55,61 @@ public class InstancePool {
 		return staticInitMap.containsKey(clazz);
 	}
 
-	public void setStaticField(KClass clazz, FieldSignature field, IObject value) {
+	public void setStaticField(KClass clazz, FieldSignature field, int[] value) {
 		System.out.println("setStaticField " + field + " " + value);
 		if (!hasStaticInit(clazz))
 			throw new IllegalStateException("Class has not been staticly initialized");
 		staticFields.put(field, value);
 	}
 
-	public IObject getStaticField(KClass clazz, FieldSignature field) {
+	public int[] getStaticField(KClass clazz, FieldSignature field) {
 		System.out.println("getStaticField " + field);
 		if (!hasStaticInit(clazz))
 			throw new IllegalStateException("Class has not been staticly initialized");
 		return staticFields.get(field);
 	}
 
-	public ObjectReference createInstance(KClass clazz) {
-		UUID id = UUID.randomUUID();
+	private int allocateObjectId() {
+		int id = objectIds.nextClearBit(idHint);
+		objectIds.set(id);
+		idHint = id + 1;
+		return id;
+	}
+
+	private void deallocateObjectId(int id) {
+		objectIds.clear(id);
+		idHint = id;
+	}
+
+	public int createInstance(KClass clazz) {
+		int id = allocateObjectId();
 		ObjectInstance instance = new ObjectInstance(id, clazz);
 		objects.put(id, instance);
-		return instance.makeReference();
+		return id;
 	}
 
-	public ObjectReference createArray(ArrayType type, int size) {
-		UUID id = UUID.randomUUID();
+	public int createArray(ArrayType type, int size) {
+		int id = allocateObjectId();
 		ObjectInstance instance = new ObjectInstance(type, id, size);
 		objects.put(id, instance);
-		return instance.makeReference();
+		return id;
 	}
 
-	public ObjectReference getRuntimeClass(ClassPool pool, IType type, KClass clazz) {
+	public int getRuntimeClass(ClassPool pool, IType type, KClass clazz) {
 		Pair<IType, Object> key = new Pair<IType, Object>(type, null);
 		if (runtimeClasses.containsKey(key)) {
-			return objects.get(runtimeClasses.get(key)).makeReference();
+			return runtimeClasses.get(key);
 		}
 
 		KClass classClass = pool.getClass("java/lang/Class", null);
-		UUID id = UUID.randomUUID();
+		int id = allocateObjectId();
 		ObjectInstance instance = new ObjectInstance(id, classClass);
 		objects.put(id, instance);
 		runtimeClasses.put(key, id);
-		return instance.makeReference();
+		return id;
 	}
 
+	public ObjectInstance getObject(int id) {
+		return objects.get(id);
+	}
 }
