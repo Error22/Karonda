8c4d9fe51a23b58c5bad78d1b3b675e6043fde54
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-02-21 20:22:26 +0000

    Convert jumpinstruction to use ints

--- src/main/java/com/error22/karonda/instructions/JumpInstruction.java
+++ src/main/java/com/error22/karonda/instructions/JumpInstruction.java
@@ -3,31 +3,11 @@ package com.error22.karonda.instructions;
 import org.objectweb.asm.Label;
 
 import com.error22.karonda.NotImplementedException;
-import com.error22.karonda.ir.ObjectReference;
-import com.error22.karonda.ir.PrimitiveObject;
-import com.error22.karonda.vm.ObjectInstance;
 import com.error22.karonda.vm.StackFrame;
 
 public class JumpInstruction implements IInstruction {
 	public static enum JumpType {
-		EqualZero,
-		NotEqualZero,
-		LessThanZero,
-		LessThanEqualZero,
-		GreaterThanEqualZero,
-		GreaterThanZero,
-		IntsEqual,
-		IntsNotEqual,
-		IntsLessThan,
-		IntsLessThanEqual,
-		IntsGreaterThanEqual,
-		IntsGreaterThan,
-		ReferenceEqual,
-		ReferenceNotEqual,
-		Subroutine,
-		Goto,
-		Null,
-		NotNull
+		EqualZero, NotEqualZero, LessThanZero, LessThanEqualZero, GreaterThanEqualZero, GreaterThanZero, IntsEqual, IntsNotEqual, IntsLessThan, IntsLessThanEqual, IntsGreaterThanEqual, IntsGreaterThan, ReferenceEqual, ReferenceNotEqual, Subroutine, Goto, Null, NotNull
 	}
 
 	private JumpType type;
@@ -43,69 +23,69 @@ public class JumpInstruction implements IInstruction {
 		boolean jump;
 		switch (type) {
 		case EqualZero:
-			jump = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue() == 0;
+			jump = stackFrame.pop() == 0;
 			break;
 		case NotEqualZero:
-			jump = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue() != 0;
+			jump = stackFrame.pop() != 0;
 			break;
 		case LessThanZero:
-			jump = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue() < 0;
+			jump = stackFrame.pop() < 0;
 			break;
 		case LessThanEqualZero:
-			jump = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue() <= 0;
+			jump = stackFrame.pop() <= 0;
 			break;
 		case GreaterThanEqualZero:
-			jump = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue() >= 0;
+			jump = stackFrame.pop() >= 0;
 			break;
 		case GreaterThanZero:
-			jump = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue() > 0;
+			jump = stackFrame.pop() > 0;
 			break;
 		case IntsEqual: {
-			int value2 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
-			int value1 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
+			int value2 = stackFrame.pop();
+			int value1 = stackFrame.pop();
 			jump = value1 == value2;
 			break;
 		}
 		case IntsNotEqual: {
-			int value2 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
-			int value1 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
+			int value2 = stackFrame.pop();
+			int value1 = stackFrame.pop();
 			jump = value1 != value2;
 			break;
 		}
 		case IntsLessThan: {
-			int value2 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
-			int value1 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
+			int value2 = stackFrame.pop();
+			int value1 = stackFrame.pop();
 			jump = value1 < value2;
 			break;
 		}
 		case IntsLessThanEqual: {
-			int value2 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
-			int value1 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
+			int value2 = stackFrame.pop();
+			int value1 = stackFrame.pop();
 			jump = value1 <= value2;
 			break;
 		}
 		case IntsGreaterThanEqual: {
-			int value2 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
-			int value1 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
+			int value2 = stackFrame.pop();
+			int value1 = stackFrame.pop();
 			jump = value1 >= value2;
 			break;
 		}
 		case IntsGreaterThan: {
-			int value2 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
-			int value1 = ((Number) ((PrimitiveObject) stackFrame.pop()).getValue()).intValue();
+			int value2 = stackFrame.pop();
+			int value1 = stackFrame.pop();
 			jump = value1 > value2;
 			break;
 		}
 		case ReferenceEqual: {
-			ObjectInstance value2 = ((ObjectReference) stackFrame.pop()).getInstance();
-			ObjectInstance value1 = ((ObjectReference) stackFrame.pop()).getInstance();
-			jump = value1.getId().equals(value2.getId());
+			int value2 = stackFrame.pop();
+			int value1 = stackFrame.pop();
+			jump = value1 == value2;
 			break;
 		}
 		case ReferenceNotEqual: {
-			ObjectInstance value2 = ((ObjectReference) stackFrame.pop()).getInstance();
-			ObjectInstance value1 = ((ObjectReference) stackFrame.pop()).getInstance();
-			jump = !value1.getId().equals(value2.getId());
+			int value2 = stackFrame.pop();
+			int value1 = stackFrame.pop();
+			jump = value1 != value2;
 			break;
 		}
 		case Subroutine:
@@ -114,10 +94,10 @@ public class JumpInstruction implements IInstruction {
 			jump = true;
 			break;
 		case Null:
-			jump = !(stackFrame.pop() instanceof ObjectReference);
+			jump = stackFrame.pop() == 0;
 			break;
 		case NotNull:
-			jump = stackFrame.pop() instanceof ObjectReference;
+			jump = stackFrame.pop() != 0;
 			break;
 		default:
 			throw new NotImplementedException();
