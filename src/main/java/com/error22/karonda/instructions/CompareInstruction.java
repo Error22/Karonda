4b63571c234b0e75ecec617dc9b7f4dc2c3db02f
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-02-23 13:16:16 +0000

    Implement compare instruction

--- src/main/java/com/error22/karonda/instructions/CompareInstruction.java
+++ src/main/java/com/error22/karonda/instructions/CompareInstruction.java
@@ -1,6 +1,5 @@
 package com.error22.karonda.instructions;
 
-import com.error22.karonda.NotImplementedException;
 import com.error22.karonda.vm.StackFrame;
 
 public class CompareInstruction implements IInstruction {
@@ -20,7 +19,59 @@ public class CompareInstruction implements IInstruction {
 
 	@Override
 	public void execute(StackFrame stackFrame) {
-		throw new NotImplementedException();
+		switch (op) {
+		case Longs: {
+			long value2 = stackFrame.popLong();
+			long value1 = stackFrame.popLong();
+			if (value1 > value2) {
+				stackFrame.push(1);
+			} else if (value1 == value2) {
+				stackFrame.push(0);
+			} else if (value1 < value2) {
+				stackFrame.push(-1);
+			} else {
+				throw new IllegalStateException();
+			}
+			break;
+		}
+		case FloatsPosNaN:
+		case FloatsNegNaN: {
+			float value2 = Float.intBitsToFloat(stackFrame.pop());
+			float value1 = Float.intBitsToFloat(stackFrame.pop());
+			if (value1 == Float.NaN || value2 == Float.NaN) {
+				stackFrame.push(op == CompareOp.FloatsNegNaN ? -1 : 1);
+			} else if (value1 > value2) {
+				stackFrame.push(1);
+			} else if (value1 == value2) {
+				stackFrame.push(0);
+			} else if (value1 < value2) {
+				stackFrame.push(-1);
+			} else {
+				throw new IllegalStateException();
+			}
+			break;
+		}
+		case DoublesPosNaN:
+		case DoublesNegNaN: {
+			double value2 = Double.longBitsToDouble(stackFrame.popLong());
+			double value1 = Double.longBitsToDouble(stackFrame.popLong());
+			if (value1 == Double.NaN || value2 == Double.NaN) {
+				stackFrame.push(op == CompareOp.DoublesNegNaN ? -1 : 1);
+			} else if (value1 > value2) {
+				stackFrame.push(1);
+			} else if (value1 == value2) {
+				stackFrame.push(0);
+			} else if (value1 < value2) {
+				stackFrame.push(-1);
+			} else {
+				throw new IllegalStateException();
+			}
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unknown op " + op);
+		}
+
 	}
 
 }
