0a64377e957e83d8d7ad717323c8a6e111875e1c
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-02-25 23:19:53 +0000

    Implement monitor instruction

--- src/main/java/com/error22/karonda/instructions/MonitorInstruction.java
+++ src/main/java/com/error22/karonda/instructions/MonitorInstruction.java
@@ -1,7 +1,8 @@
 package com.error22.karonda.instructions;
 
-import com.error22.karonda.NotImplementedException;
+import com.error22.karonda.vm.KThread;
 import com.error22.karonda.vm.StackFrame;
+import com.error22.karonda.vm.ThreadManager;
 
 public class MonitorInstruction implements IInstruction {
 	public static enum MonitorOperation {
@@ -17,7 +18,30 @@ public class MonitorInstruction implements IInstruction {
 
 	@Override
 	public void execute(StackFrame stackFrame) {
-		throw new NotImplementedException();
+		KThread thread = stackFrame.getThread();
+		ThreadManager manager = thread.getThreadManager();
+
+		int ref = stackFrame.pop();
+		KThread owner = manager.getLockOwner(ref);
+
+		switch (op) {
+		case Enter:
+			if (owner != thread && owner != null) {
+				stackFrame.moveInstructionPointer(-1);
+				stackFrame.push(ref, true);
+				return;
+			}
+			thread.lock(ref);
+			break;
+		case Exit:
+			if (owner != thread) {
+				throw new IllegalStateException("Current thread does not own object lock");
+			}
+			thread.unlock(ref);
+			break;
+		default:
+			break;
+		}
 	}
 
 }
