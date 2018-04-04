2a1b049415d68a7ada8ac81248b1bd9314b39c07
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-04-04 20:58:38 +0100

    Implement switch instruction

--- src/main/java/com/error22/karonda/instructions/SwitchInstruction.java
+++ src/main/java/com/error22/karonda/instructions/SwitchInstruction.java
@@ -2,7 +2,6 @@ package com.error22.karonda.instructions;
 
 import org.objectweb.asm.Label;
 
-import com.error22.karonda.NotImplementedException;
 import com.error22.karonda.vm.StackFrame;
 
 public class SwitchInstruction implements IInstruction {
@@ -12,12 +11,20 @@ public class SwitchInstruction implements IInstruction {
 
 	public SwitchInstruction(Label defaultLabel, int[] keys, Label[] labels) {
 		this.defaultLabel = defaultLabel;
+		this.keys = keys;
 		this.labels = labels;
 	}
 
 	@Override
 	public void execute(StackFrame stackFrame) {
-		throw new NotImplementedException();
+		int value = stackFrame.pop();
+		for (int i = 0; i < keys.length; i++) {
+			if (keys[i] == value) {
+				stackFrame.jump(labels[i]);
+				return;
+			}
+		}
+		stackFrame.jump(defaultLabel);
 	}
 
 }
